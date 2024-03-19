c03cd110982b76de3d97ae6783c20434da3d537f
almursalat <mursalatjamal@gmail.com>
2024-03-19 22:30:19 +0800

    first comit

---
+++ algoritmapseudocode.js
@@ -0,0 +1,55 @@
+// ALGORITMA PSEUDOCODE
+
+/*
+Study case 1
+
+algoritma:
+1. buat variable panjang
+2. buat variable lebar
+3. buat variable keliling
+4. masukkan rumus keliling = 2 x panjang + 2 x lebar
+5. cetak keliling
+
+pseudocode:
+isi panjang dengan angka
+isi lebar dengan angka
+
+isi keliling dengan 2 x panjang ditambah 2 x lebar
+cetak keliling 
+
+study case 2
+
+algoritma:
+1. cari luas permukaan tabung = 2 * phi * r * r + 2 * phi * r * t
+2. buat variable phi
+3. buat variable r
+4. buat variable t
+5. buat variable luasPermukaan
+6. isi luasPermukaan dengan rumus = 2 * phi * r * r + 2 * phi * r * t
+7. cetak luasPermukaan
+
+pseudocode:
+isi phi dengan 3.14
+isi r dengan angka
+isi t dengan angaka
+
+isi luasPermukaan dengan 2 * phi * r * r + 2 * phi * r * t
+cetak luasPermukaan
+
+study case 3
+
+algoritma:
+1. cari rumus volume bola v = 4/3 * phi * r * r * r
+2. buat variable phi
+3. buat variable r 
+4. buat variable volume
+5. buat volume dengan rumus = 1/4 * 4/3 * phi * r * r * r
+6. cetak volume
+
+pseudocode:
+isi phi dengan 3.14
+isi r dengan angka
+
+isi volume dengan 1/4 * 4/3 * phi * r * r * r
+cetak volume
+*/
---
+++ variabletipedata.js
@@ -0,0 +1,26 @@
+// VARIABLE dan RIPE DATA
+
+// var number = 10;
+// console.log(number);
+
+// var str = "buku";
+// console.log(str);
+
+// var isCorrect = true;
+// console.log(isCorrect);
+
+// OPERATOR MATEMATIKA
+var a = 10;
+var b = 'xyz';
+var total = a - b;
+
+console.log(typeof total);
+
+// OPERATOR LOGIKA
+// == membandingkan value saja
+// === membandingkan value dan tipe data
+var x = 10;
+var y = 10;
+
+var z = x == y;
+console.log(z);
