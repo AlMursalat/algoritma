d6b206d0aef321af6282181c5465221487f12876
almursalat <mursalatjamal@gmail.com>
2024-04-03 20:36:07 +0800

    tes1

--- testes.js
+++ testes.js
@@ -1,7 +1,14 @@
 class Tes {
-  constructor(address, name, address) {
+  constructor(id, name, address) {
     this.id = id;
     this.name = name;
     this.address = address;
+  }
+}
+
+class Anak1 extends Tes{
+  constructor(id, name, address, age){
+    super(id, name, address)
+    this.age = age;
   }
 }
